521dcc2545b5aeed773ca0bc1501362a00646bf6
David Wilson and Jordan Loop <david@>
2017-04-26 09:45:34 -0700

    Add basic HTML with links for writing yourself a letter.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
   $("#formOne").submit(function(event){
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput = $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+    var firstNameInput = $("input#firstName").val();
+    var lastNameInput = $("input#lastName").val();
+    var addressInput = $("input#address").val();
+    var cityInput = $("input#city").val();
+    var stateInput = $("input#state").val();
+    var zipInput = $("input#zip").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    $(".firstName").text(firstNameInput);
+    $(".lastName").text(lastNameInput);
+    $(".address").text(addressInput);
+    $(".city").text(cityInput);
+    $(".state").text(stateInput);
+    $(".zip").text(zipInput);
 
-    $("#story").show();
+    $("#letter").show();
 
     event.preventDefault();
     });
